9d1d2c00ef7d857a624d07d112e1cadbd35f7869
skdishansachin <skdishansachin@gmail.com>
2025-01-30 21:45:44 +0530

    wip

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,38 +1,50 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            Create post
-        </h2>
+        Create post
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    <form method="post" action="{{ route('posts.store') }}" class="space-y-6">
-                        @csrf
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <form method="post" action="{{ route('posts.store') }}">
+                    @csrf
 
-                        <div>
-                            <x-input-label for="title" :value="__('Title')" />
-                            <x-text-input id="title" name="title" type="text" class="mt-1 block w-full" :value="old('title')" required autofocus />
-                            <x-input-error class="mt-2" :messages="$errors->get('title')" />
-                        </div>
+                    <div>
+                        <x-input-label for="title" :value="__('Title')" />
+                        <x-text-input id="title" name="title" type="text" class="mt-1 block w-full" :value="old('title')" required autofocus />
+                        <x-input-error class="mt-2" :messages="$errors->get('title')" />
+                    </div>
 
-                        <div>
-                            <x-input-label for="image" :value="__('Image')" />
-                            <input type="file" name="image" id="image" class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" />
-                            <x-input-error class="mt-2" :messages="$errors->get('image')" />
+                    <div class="mt-6">
+                        <x-input-label for="image" :value="__('Image')" />
+                        <div class="col-span-full">
+                            <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-5">
+                                <div class="text-center">
+                                    <div class="mt-4 flex text-sm/6 text-gray-600">
+                                        <label for="image" class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:outline-hidden hover:text-indigo-500">
+                                            <span>Upload a file</span>
+                                            <input id="image" name="image" type="image" class="sr-only" />
+                                        </label>
+                                        <p class="pl-1">or drag and drop</p>
+                                    </div>
+                                    <p class="text-xs/5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                                </div>
+                            </div>
                         </div>
+                        <x-input-error class="mt-2" :messages="$errors->get('image')" />
+                    </div>
 
-                        <div>
-                            <x-input-label for="content" :value="__('Content')" />
-                            <textarea id="content" name="content" type="text" class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
-                                {{ old('content') }}
-                            </textarea>
-                            <x-input-error class="mt-2" :messages="$errors->get('content')" />
-                        </div>
-                    </form>
-                </div>
+                    <div class="mt-6">
+                        <x-input-label for="content" :value="__('Content')" />
+                        <textarea id="content" name="content" rows="6" type="text" class="mt-1 block w-full border-gray-300 focus:border-indigo-500  focus:ring-indigo-500 rounded-md shadow-sm">
+                        {{ old('content') }}
+                        </textarea>
+                        <x-input-error class="mt-2" :messages="$errors->get('content')" />
+                    </div>
+
+                    <div class="mt-6">
+                        <x-primary-button>Create</x-primary-button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
