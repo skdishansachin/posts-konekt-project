c9724a00178bcbdcd5d9dbbe24f43bb8896be10b
skdishansachin <skdishansachin@gmail.com>
2025-01-30 20:28:41 +0530

    feat: organize API routes under versioning and improve middleware grouping

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,14 @@ use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    Route::post('/login', [LoginController::class, 'store']);
-});
+Route::group(['prefix' => 'v1'], function () {
+    Route::middleware('guest')->group(function () {
+        Route::post('/login', [LoginController::class, 'store']);
+    });
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [LoginController::class, 'destroy']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [LoginController::class, 'destroy']);
 
-    Route::apiResource('posts', PostController::class);
+        Route::apiResource('posts', PostController::class);
+    });
 });
