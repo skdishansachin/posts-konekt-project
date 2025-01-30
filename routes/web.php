21ea69c6fec13da35998c0ab79dd7b1b8f9d86ac
skdishansachin <skdishansachin@gmail.com>
2025-01-30 14:27:51 +0530

    feat: simplify post routes by using resource controller for PostController

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,7 @@ Route::middleware('auth')->group(function () {
     // TODO: Chnage the logout route to POST request
     Route::get('logout', [LoginController::class, 'destroy'])->name('logout');
 
-    Route::get('posts', [PostController::class, 'index'])->name('posts.index');
-    Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
-    Route::post('posts', [PostController::class, 'store'])->name('posts.store');
-    Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
-    Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-    Route::put('posts/{post}', [PostController::class, 'update'])->name('posts.update');
-    Route::delete('posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::resource('posts', PostController::class);
 });
 
 Route::get('dashboard', function () {
