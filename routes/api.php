ef4a39a97d3ff7cd9f614fd137f26bdcd8fb8234
skdishansachin <skdishansachin@gmail.com>
2025-01-30 14:03:57 +0530

    feat: update API routes to include login and logout functionality with authentication middleware

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,4 +9,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::resource('posts', PostController::class);
+Route::middleware('guest')->group(function () {
+    Route::post('/login', [LoginController::class, 'store']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [LoginController::class, 'destroy']);
+
+    Route::apiResource('posts', PostController::class);
+});
